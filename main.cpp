183a150adfbd177aac34a7d22fcfaab4cfa8092f
shaerthomas <thomas@DESKTOP-3L1MQFD.localdomain>
2022-05-21 00:50:40 +0100

    Got basic node example working with compile timer parser

--- main.cpp
+++ main.cpp
@@ -3,14 +3,22 @@
 #include <string>
 #include "ctpg.hpp"
 #include <iostream>
+#include "nodes.cpp"
 #include <charconv>
 
 
 using namespace ctpg;
 using namespace ctpg::buffers;
 
+//Nonterminal symbols (short: nonterms) are essentially all non atomic symbols in the grammar. In C++ language these are things like: expression, class definition, function declaration etc.
+// int is value type
 constexpr nterm<int> list("list");
 
+
+constexpr nterm<BinOp> binop("binop");
+
+
+//Terminal symbols (short: terms) are symbols used in grammar definition that are atomic blocks. Examples of the terms from a C++ language are: identifier, '+' operator, various keywords etc.
 constexpr char number_pattern[] = "[1-9][0-9]*";
 constexpr regex_term<number_pattern> number("number");
 
@@ -22,45 +30,42 @@ constexpr int to_int(std::string_view sv)
     return sum;
 }
 
+/*
+The parser class together with its template deduction guides allows to define parsers using 4 arguments:
 
+Grammar root - symbol which is a top level nonterm for a grammar.
+List of all terms
+List of all nonterms
+List of rules
+
+
+List of terms enclosed in a terms call. In our case there are two: number and a ,.
+
+Note: the , term is not defined earlier in the code. It is an implicit char_term. The code implicitly converts the char to the char_term class. Therefore char_terms (as well as string_terms) are allowed not to be defined in advance. Their debug names are assigned to the them by default to a char (or a string) they represent.
+
+Nonterm list.
+
+List of terms enclosed in a nterms call. In our case, just a single list nonterm is enough.
+
+Rules
+
+List of rules enclosed in a rules call. Each rule is in the form of: nonterm(symbols...) >= functor The nonterm part is what's called a left side of the rule. The symbols are called the right side.
+
+The right side can contain any number of nterm objects as well as terms (regex_terms, char_terms or string_terms). Terms can be in their implicit form, like , in the example. Implicit string_terms are in form of "strings".
+*/
 constexpr parser p(
-    list,
-    terms(',', number),
-    nterms(list),
+    binop,
+    terms('+', number),
+    nterms(list, binop),
     rules(
-        list(number) >=
-            to_int,
-        list(list, ',', number)
-            >= [](int sum, char, const auto& n){ return sum + to_int(n); }
+        // list(number) >=
+        //     to_int,
+        // list(list, ',', number)
+        //     >= [](int sum, char, const auto& n){ return 5.23; }
+         binop(number, '+', number)
+             >= [](const auto& n, char, const auto& n2){ return BinOp(to_int(n), to_int(n2)); }
     )
 );
-
-class Node {
-public:
-
-    const int a = 2;
-private:
-
-};
-
-class Number : public Node {
-public:
-
-    constexpr Number(const int _value) : value (_value) {}
-
-    const int value;
-
-private:
-
-};
-
-
-class BinOp {
-public:
-
-private:
-
-};
 
 
 // https://en.cppreference.com/w/cpp/meta
@@ -73,44 +78,18 @@ private:
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
-    {
-        constexpr char example_text[] = "1, 20, 3";
 
-        constexpr auto cres = p.parse(cstring_buffer(example_text)); // notice cstring_buffer and no std::err output
-        std::cout << cres.value() << std::endl;
-        return 0;
-    }
+    constexpr char example_text[] = "1 + 40";
 
-    auto res = p.parse(string_buffer(argv[1]), std::cerr);
-    bool success = res.has_value();
+    constexpr auto cres = p.parse(cstring_buffer(example_text)); // notice cstring_buffer and no std::err output
+    // constexpr int result = cres.value();
+    // static_assert(result == 5);
+
+    bool success = cres.has_value();
     if (success)
-        std::cout << res.value() << std::endl;
-    return success ? 0 : -1;
+        std::cout << cres.value().evaluate() << std::endl;
+    
+    return 0;
+
 }
-
-
-// int main() {
-
-//     constexpr const Number n(5);
-//     static_assert(n.value == 5);
-
-//     constexpr int a = 3 + 2;
-    
-//     const static Node node;
-
-//     static const constexpr Node* nodepointer = &node;
-    
-//     //const Node* b = new Node();
-//     static_assert(a == 5);
-//     std::cout << "hello" << std::endl;
-//     //constexpr const int extract = nodepointer->a;
-//     //static_assert(extract == 2);
-//     //static_assert(nodepointer->a == 2);
-//     //static_assert(nodepointer != nullptr);
-
-//     //static_assert(a != 5);
-
-// }
-
 
