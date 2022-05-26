5af55ae7a72c88ef146cd635f684ba8bd28f42da
shaerthomas <thomas@DESKTOP-3L1MQFD.localdomain>
2022-05-26 01:17:31 +0100

    Began testing linked list implementation in preparation for compile time symbol table

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include <charconv>
+#include <functional>
 
 
 
@@ -10,29 +11,89 @@
 
 template<int N>
 struct Num {
-     static const int value = N;
-};
+    //  static const int value = N;
+    // using value = N;
 
-
-
-template<typename A, typename B>
-struct BinOp {
-    // static const int value = A::value + B::value;
-
-    static constexpr const int evaluate() {
-        return A::value + B::value;
+     static constexpr const int interpret() {
+        return N;
     }
 };
 
 
 
+template<typename A, typename B>
+struct BinOpPlus {
+    // static const int value = A::value + B::value;
+    // using value = Num<A::value + B::value>;
+
+    static constexpr const int interpret() {
+        return A::interpret() + B::interpret();
+    }
+};
+
+
+
+struct LinkedListEmptyNode {
+
+};
+
+
+// template<typename T>
+// struct LinkedListNode {
+// };
+
+
+template<int V, typename T = LinkedListEmptyNode>
+struct LinkedListNode {
+    using nextType = T;
+    static const int value = V;
+};
+
+
+template<typename T, typename A>
+struct TestType {
+
+};
+
+
+template<typename T>
+struct LinkedListWalker {
+    static const int value  = 8;
+};
+
+/*
+Has next node
+*/
+template<template <typename, typename ...> typename A, typename T, typename ...Args>
+struct LinkedListWalker<A<T, Args...>> {
+    static const int value  = 7;
+};
+
+
+
+
+//https://stackoverflow.com/questions/6793259/how-does-one-implement-hash-tables-in-a-functional-language
+
+
 int main(int argc, char* argv[])
 {
 
-    using expression = BinOp<Num<2>, Num<5>>;
+    using test = LinkedListNode<2, LinkedListNode<3, LinkedListNode<-1>>>;
 
-    constexpr int result = expression::evaluate();
-    static_assert(result == 7);
+    constexpr int result2 = LinkedListWalker<TestType<int, int>>::value;
+    
+
+    std::cout << result2 << std::endl;
+    // static_assert(result2 == 3);
+
+
+    return 0;
+
+    // using expression = BinOpPlus<Num<2>, BinOpPlus<Num<2>, Num<5>>>;
+    using expression = BinOpPlus<Num<2>, Num<7>>;
+
+    constexpr int result = expression::interpret();
+    static_assert(result == 9);
 
     // static_assert(expression::value == 7);
 
