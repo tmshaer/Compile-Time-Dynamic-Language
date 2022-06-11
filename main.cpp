e1853519b4852e39573d194dbcafe8dec703ced1
shaerthomas <thomas@DESKTOP-3L1MQFD.localdomain>
2022-06-11 21:15:34 +0100

    Rudimentary implementation of linked list into node interpretation

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <charconv>
 #include <functional>
-#include "lookuptable.h"
 #include "node.h"
 #include "genericlookuptable.h"
 #include "string"
@@ -17,13 +16,34 @@ int main(int argc, char* argv[])
 {
 
 
+    /*
+    TODO.
+    Write tests for linkedlistgetvalue, linkedlistsetvalue, linkedlistaddvalue
+    such that they work on a empty lists, 1 element lists, n element lists
 
-    using storage = LinkedListNode<"a", 3, LinkedListNode<"b", 4, LinkedListNode<"hey!", 9>>>;
+    Make it so that you don't have to explicitly pass in a linkedlist item to Execute
+
+    Make a function to either update a linkedlist if value exists else just append new one 
+    (because no syntactic difference between assign and vardecl)    
+    */
+
+using storage = LinkedListNode<"a", 3, LinkedListNode<"b", 4, LinkedListNode<"hey!", 9>>>;
 
 
     std::cout << LinkedListGetValue<storage, "a">::value << std::endl;
     std::cout << LinkedListGetValue<LinkedListSetValue<storage, "a", 40>::newList, "a">::value << std::endl;
     std::cout << LinkedListGetValue<LinkedListAddValue<storage, "new", 90>::newList, "new">::value << std::endl;
+
+
+    using code = Execute<storage,
+                         Assign<"x", Num<6>>,
+                         Assign<"y", BinOpPlus<Num<2>, Num<7>>>,
+                         Assign<"z", BinOpPlus<BinOpPlus<Num<2>, Num<10>>, Num<7>>>>;
+
+
+    std::cout << LinkedListGetValue<code::values, "x">::value << std::endl;
+    std::cout << LinkedListGetValue<code::values, "y">::value << std::endl;
+    std::cout << LinkedListGetValue<code::values, "z">::value << std::endl;
 
 
 
@@ -32,7 +52,7 @@ int main(int argc, char* argv[])
     // using expression = BinOpPlus<Num<2>, BinOpPlus<Num<2>, Num<5>>>;
     using expression = BinOpPlus<Num<2>, Num<7>>;
 
-    constexpr int result = expression::interpret();
+    constexpr int result = expression::value;
     static_assert(result == 9);
 
     // static_assert(expression::value == 7);
--- node.h
+++ node.h
@@ -1,3 +1,8 @@
+#include "lookuptable.h"
+
+
+
+
 /*
 Contains all the valid AST nodes for our language.
 */
@@ -5,22 +10,41 @@ Contains all the valid AST nodes for our language.
 
 template<int N>
 struct Num {
-    //  static const int value = N;
-    // using value = N;
-
-     static constexpr const int interpret() {
-        return N;
-    }
+     static const constexpr int value = N;
 };
 
 
 
 template<typename A, typename B>
 struct BinOpPlus {
-    // static const int value = A::value + B::value;
-    // using value = Num<A::value + B::value>;
-
-    static constexpr const int interpret() {
-        return A::interpret() + B::interpret();
-    }
+    static const constexpr int value = A::value + B::value;
 };
+
+
+
+
+template<string_literal VarName, typename Value, typename SymbolTable = LinkedListEmptyNode>
+struct Assign {
+    using UpdatedSymbolTable = LinkedListAddValue<SymbolTable, VarName, Value::value>::newList;
+};
+
+
+
+// variadic recursion ... pass symbol table as variable to each one
+
+
+template <typename SymbolTable, typename... Statements>
+struct Execute {
+    using values = SymbolTable;
+};
+
+
+template <typename SymbolTable, template <string_literal, typename> typename T, string_literal VarName, typename Value, typename... Statements>
+struct Execute<SymbolTable, T<VarName, Value>, Statements...> {
+
+    using TempTable = Assign<VarName, Value, SymbolTable>::UpdatedSymbolTable;
+    using values = Execute<TempTable, Statements...>::values;
+};
+
+
+
